742bd4e2ac7d4252ffd22c4886cbfe8ce613c18e
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-09 14:29:38 +0000

    229

--- config/routes.rb
+++ config/routes.rb
@@ -9,25 +9,25 @@ Rails.application.routes.draw do
   devise_for :admins,contorollers:{
    sessions: "adomin/sessions"
   }
-  namespace 
 
    namespace :admin do
    root :to => "homes#top"
-   resources :member,only: [:index,:show,:edit,:update]
-   resources :
+   resources :members, only: [:index,:show,:edit,:update]
+   resources :games, only: [:index,:show,:edit,:update]
+   resources :category, only[:index,:show,:edit,:update]
  end
- 
+
  scope module: :public do
    root :to =>"homes#top"
    get "about" => "homes#top"
-   resources :member,only: [:index,:show,:edit,:update]
+   resources :member, only: [:index,:show,:edit,:update]
    resource :relationships, only: [:create, :destroy]
    get 'followings' => 'relationships#followings', as: 'followings'
    get 'followers' => 'relationships#followers', as: 'followers'
-   resources :game,only: [:index,:create,:show,:update,:destroy]
-   resources :coments,only: [:create,:destroy]
-   resources :categry
-   resource :favorites,only[:create,:destroy]
+   resources :game, only: [:index,:create,:show,:update,:destroy]
+   resources :coments, only: [:create,:destroy]
+   resources :category, only: [:index,:create,:show,:update]
+   resource :favorites, only[:create,:destroy]
  end
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
