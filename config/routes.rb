263e6ab7b28e7271d340fc0f9df199b9683b149b
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-06-09 06:40:04 +0000

    22

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  #ユーザー用
+  devise_for :members, contorollers:{
+    registrstions: "publick/ registrstions",
+    sessions: 'publick/sessions'
+  }
+  #管理者用
+  devise_for :admins
+   sessions: "adomin/sessions"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
